f113a4cadd9ef16c53e50940c4eba8b8139f31b8
Yanick <nckmuhire@gmail.com>
2025-09-11 20:31:54 +0200

    add User authentication templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from . import views
 from .views import LibraryDetailView
-from .views import list_books
 
 urlpatterns = [
-    # Function-based view to list all books
+    # Function-based view for books
     path('books/', views.list_books, name='list_books'),
 
     # Class-based view for library details
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    # User authentication
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
