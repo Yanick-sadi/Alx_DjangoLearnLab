232380e3e0d7780b190aea33d40f1279ceebf318
Yanick <nckmuhire@gmail.com>
2025-09-11 18:21:07 +0200

    add templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,5 +10,5 @@ def list_books(request):
 # Class-based view to display details for a specific library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "relationship_app/library_detail.html"  # <-- FIXED explicit reference
+    template_name = "relationship_app/library_detail.html"  
     context_object_name = "library"
