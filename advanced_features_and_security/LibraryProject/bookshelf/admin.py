41030c012d1d076ae1c33371ee89ea9d60226925
Yanick <nckmuhire@gmail.com>
2025-09-18 07:21:49 +0200

    update bookshelf admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,17 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Book
 
-# Register your models here.
-from .models import Book
+# ----------------------------
+# Custom User Admin
+# ----------------------------
+class CustomUserAdmin(UserAdmin):
+    # Fields to display in the admin panel
+    list_display = ("username", "email", "first_name", "last_name", "is_staff", "date_of_birth")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+    
+    # Fields to use when adding/editing a user
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email", "date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "is_staff", "is_superuser"),
+        }),
+    )
+    
+    search_fields = ("username", "email")
+    ordering = ("username",)
+    filter_horizontal = ("groups", "user_permissions")
 
-class BookAdmin(admin.ModelAdmin):
-    # Columns to show in the list view
-    list_display = ("title", "author", "publication_year")
 
-    # Add search bar for these fields
-    search_fields = ("title", "author")
-
-    # Add filter sidebar for publication_year
-    list_filter = ("publication_year",)
-
-# Register the model + custom admin
-admin.site.register(Book, BookAdmin)
+# ----------------------------
+# Register models
+# ----------------------------
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Book)
