8ab76ecdcc4e95165cd07d05ec6d73eacd6e1be1
Yanick <nckmuhire@gmail.com>
2025-09-25 12:24:00 +0200

    add BookViewSet class and register BookViewSet

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,6 +3,12 @@ from .views import BookListCreateView, BookDetailView
 from django.contrib import admin
 from django.urls import path, include
 from .views import BookList
+from .views import BookViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookListCreateView.as_view(), name='book-list-create'),
@@ -10,6 +16,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')), 
     path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 
 ]
 
