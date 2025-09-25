5654645c65a1734f12603aa728649467e4edf17f
Yanick <nckmuhire@gmail.com>
2025-09-25 12:11:35 +0200

    add Book class to view, updated urls.py in both api and api_project

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from .views import BookListCreateView, BookDetailView
 from django.contrib import admin
 from django.urls import path, include
+from .views import BookList
 
 urlpatterns = [
     path('books/', BookListCreateView.as_view(), name='book-list-create'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')), 
+    path('books/', BookList.as_view(), name='book-list'),
 
 ]
 
