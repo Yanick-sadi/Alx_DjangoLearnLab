e8c82a886ec60768a44bf098f67da4982f3cdd76
Yanick <nckmuhire@gmail.com>
2025-10-13 20:46:10 +0200

    update the Urls.py to add configuration of Url

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,21 +5,34 @@ app_name = 'api'
 
 urlpatterns = [
     
+    # Book ListView - for retrieving all books
     path('books/', views.BookListView.as_view(), name='book-list'),
+    
+    # Book CreateView - for adding a new book
     path('books/create/', views.BookCreateView.as_view(), name='book-create'),
     
+    # Book DetailView - for retrieving a single book by ID
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
-    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete'),
+    
+    # Book UpdateView - for modifying an existing book
+    path('books/update/<int:pk>/', views.BookUpdateView.as_view(), name='book-update'),
+    
+    # Book DeleteView - for removing a book
+    path('books/delete/<int:pk>/', views.BookDeleteView.as_view(), name='book-delete'),
     
     
+    # Author ListView
     path('authors/', views.AuthorListView.as_view(), name='author-list'),
+    
+    # Author CreateView
     path('authors/create/', views.AuthorCreateView.as_view(), name='author-create'),
     
+    # Author DetailView
     path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author-detail'),
-    path('authors/<int:pk>/update/', views.AuthorUpdateView.as_view(), name='author-update'),
-    path('authors/<int:pk>/delete/', views.AuthorDeleteView.as_view(), name='author-delete'),
     
-   path('legacy/books/', views.BookListView.as_view(), name='legacy-book-list'),
-    path('legacy/authors/', views.AuthorListView.as_view(), name='legacy-author-list'),
+    # Author UpdateView
+    path('authors/update/<int:pk>/', views.AuthorUpdateView.as_view(), name='author-update'),
+    
+    # Author DeleteView
+    path('authors/delete/<int:pk>/', views.AuthorDeleteView.as_view(), name='author-delete'),
 ]
