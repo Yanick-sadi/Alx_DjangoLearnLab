538312866d26e9e5f88e36e4c5ead7179de1d3b9
Yanick <nckmuhire@gmail.com>
2025-10-16 01:14:27 +0200

    Update Views to add CRUD Operations URLs for CRUD Operations created additional templates for CRUD operations update styling

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,25 @@
 from django.urls import path
-from django.views.generic import TemplateView  # Add this import
+from django.views.generic import TemplateView
 from . import views
+from .views import (
+    PostListView, 
+    PostDetailView, 
+    PostCreateView, 
+    PostUpdateView, 
+    PostDeleteView
+)
+
+app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='posts'),
+    # Class-based views for CRUD operations
+    path('', PostListView.as_view(), name='posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    
+    # Authentication pages
     path('login/', TemplateView.as_view(template_name='blog/login.html'), name='login'),
     path('register/', TemplateView.as_view(template_name='blog/register.html'), name='register'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
+# List View - Display all posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+    paginate_by = 5
+
+# Detail View - Display single post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+# Create View - Create new post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# Update View - Update existing post
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# Delete View - Delete post
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:posts')
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# Function-based view for posts (keeping for compatibility)
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
