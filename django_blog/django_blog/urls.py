9583ba12f9a1428684a7838e3bcbe8f384353ba0
Yanick <nckmuhire@gmail.com>
2025-10-16 01:15:46 +0200

    update Main Urls

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -21,5 +21,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='blog/home.html'), name='home'),
-    path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
