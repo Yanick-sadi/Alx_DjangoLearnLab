1da3cbef448dfc92dce2efec2e1ef3918f3af42a
Yanick <nckmuhire@gmail.com>
2025-10-16 23:18:23 +0200

    create Authentication forms, update models for profile create authentication views and templates

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from PIL import Image
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -13,3 +14,21 @@ class Post(models.Model):
     
     class Meta:
         ordering = ['-published_date']
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    bio = models.TextField(max_length=500, blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+        # Resize image if it's too large
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,11 +1,58 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib import messages
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Profile
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
-# List View - Display all posts
+# Authentication Views
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+class CustomLoginView(LoginView):
+    template_name = 'blog/login.html'
+    
+    def form_valid(self, form):
+        messages.success(self.request, f'Welcome back, {form.get_user().username}!')
+        return super().form_valid(form)
+
+class CustomLogoutView(LogoutView):
+    template_name = 'blog/logout.html'
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'blog/profile.html', context)
+
+# Blog CRUD Views (existing, but updated with authentication)
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
@@ -13,23 +60,21 @@ class PostListView(ListView):
     ordering = ['-published_date']
     paginate_by = 5
 
-# Detail View - Display single post
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
 
-# Create View - Create new post
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'blog/post_form.html'
     fields = ['title', 'content']
-    success_url = '/blog/'  # Use absolute URL
+    success_url = '/blog/'
     
     def form_valid(self, form):
         form.instance.author = self.request.user
+        messages.success(self.request, 'Your post has been created!')
         return super().form_valid(form)
 
-# Update View - Update existing post
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = 'blog/post_form.html'
@@ -37,26 +82,25 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     
     def form_valid(self, form):
         form.instance.author = self.request.user
+        messages.success(self.request, 'Your post has been updated!')
         return super().form_valid(form)
     
     def get_success_url(self):
-        return f'/blog/post/{self.object.pk}/'  # Redirect to post detail
+        return f'/blog/post/{self.object.pk}/'
     
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
 
-# Delete View - Delete post
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
-    success_url = '/blog/'  # Use absolute URL
+    success_url = '/blog/'
     
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
 
-# Keep function-based view for compatibility
 def post_list(request):
     posts = Post.objects.all().order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
