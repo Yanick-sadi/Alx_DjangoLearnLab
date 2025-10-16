e66ac2a7f0299b179de19e4f3eb1776ed2790a10
Yanick <nckmuhire@gmail.com>
2025-10-16 23:41:32 +0200

    update URL Configuration, templates and styling

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,12 +7,12 @@ from .views import (
 )
 
 urlpatterns = [
-    # Blog CRUD URLs
+    # Blog Post CRUD URLs
     path('', PostListView.as_view(), name='post_list'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('post/new/', PostCreateView.as_view(), name='post_create'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('posts/new/', PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     
     # Authentication URLs
     path('register/', register, name='register'),
